6d40a5a5d46e387b1784fee9017cd3c5e8190ae1
twer <tj19832@gmail.com>
2013-06-05 00:02:10 +0800

    jtong - introduce context

--- spec/would_spec.js
+++ spec/would_spec.js
@@ -53,7 +53,73 @@ describe('Logic Would',function(){
         });
 
 
+        describe('when condition combinator while judging',function(){
+            var condition_a_handle;
+            var condition_b_handle;
+
+            it("should do when given all conditions is true",function(){
+                var a = function(){
+                    return false;
+                };
+                var b = function(){
+                    return false;
+                };
+
+                var a_handler = function(){
+                    condition_a_handle = "a not null";
+                }
+
+                var b_handler = function(){
+                    condition_b_handle = "b not null";
+                }
+
+                var condition_a = condition(a).when_not_pass_call(a_handler);
+                var condition_b = condition(b).when_not_pass_call(b_handler);
+
+                would(be_called)
+                    .when(all(condition_a,condition_b).pass)();
+
+                expect(called_flag).toBe(false);
+                expect(condition_a_handle).toBe("a not null");
+                expect(condition_b_handle).toBe("b not null");
+            });
+
+            describe('when condition combinator while judging on context',function(){
+                var condition_a_handle_out_context;
+                var condition_b_handle_out_context;
+
+                it("should do when given all conditions is true",function(){
+                    var a = function(){
+                        return false;
+                    };
+                    var b = function(){
+                        return false;
+                    };
+
+                    var a_handler = function(){
+                        condition_a_handle_out_context = "a not null";
+                    }
+
+                    var b_handler = function(){
+                        condition_b_handle_out_context =  "b not null";
+                    }
+                    on_context(function(it){
+                        it.has_condition(a).when_not_pass_call(a_handler);
+                        it.has_condition(b).when_not_pass_call(b_handler);
+                        would(be_called)
+                            .when(all(conditions_in(it)).pass)();
+                    });
+
+
+                    expect(called_flag).toBe(false);
+                    expect(condition_a_handle_out_context).toBe("a not null");
+                    expect(condition_b_handle_out_context).toBe("b not null");
+                });
+            });
+        });
+
     });
+
 
 
 })
--- src/would.js
+++ src/would.js
@@ -1,3 +1,23 @@
+function Context(){
+    this.conditions = [];
+}
+
+Context.prototype.has_condition = function(condition){
+    this.conditions.push(condition);
+    condition.when_not_pass_call = function(handler){
+        condition.handler = handler;
+        return condition;
+    }
+    return condition;
+}
+var on_context = function(express_in_context){
+    express_in_context(new Context);
+}
+
+var conditions_in = function(context){
+    return context.conditions;
+}
+
 var when = function(condition){
   if(condition()){
       return this.action;
@@ -16,14 +36,32 @@ var would = function(action){
 
 var all = function(){
     var result = {}
-    var condition_arguments = arguments;
+    var conditions = []
+    if(arguments.length == 1){
+        conditions = [].concat(arguments[0])
+    } else{
+        conditions = arguments;
+    }
     result.pass = function(){
         var pass_or_not = false;
-
-        for (index in condition_arguments){
-            pass_or_not |= condition_arguments[index]();
+        for (var index in conditions){
+            var current_pass = conditions[index]();
+            pass_or_not = pass_or_not || current_pass;
+            if(!current_pass && conditions[index].handler){
+                conditions[index].handler();
+            }
         }
         return pass_or_not;
     }
     return result;
 }
+
+var condition = function(condition_function){
+  var result = condition_function;
+  result.when_not_pass_call = function(handler){
+      result.handler = handler;
+      return result;
+  }
+  return result;
+}
+
