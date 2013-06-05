2eeadbf2d37a2415e9796c46ba8453b1911634b3
twer <tj19832@gmail.com>
2013-06-05 23:51:36 +0800

    jtong - extroduce sub class AllConditions of Conditions

--- src/relations.js
+++ src/relations.js
@@ -1,3 +1,9 @@
+var all = function(){
+    var result = new AllConditions();
+    result.init(arguments);
+    return result;
+}
+
 function Conditions(){
     this.conditions = [];
 }
@@ -9,50 +15,48 @@ Conditions.prototype.init = function(args){
         this.conditions = args;
     }
 }
-
-var all = function(){
-    var result = new Conditions();
-    result.init(arguments)
-
-    result.go_through_conditions = function(all_pass_judge) {
-        var conditions = this.conditions;
-        for (var index in conditions) {
-            var condition = conditions[index];
-            var current_pass = condition();
-            all_pass_judge(current_pass);
-            if (!current_pass && condition.not_pass_handler) {
-                condition.not_pass_handler();
-            }
-            if (current_pass && condition.pass_handler) {
-                condition.pass_handler();
-            }
+Conditions.prototype.go_through_conditions = function(all_pass_judge) {
+    var conditions = this.conditions;
+    for (var index in conditions) {
+        var condition = conditions[index];
+        var current_pass = condition();
+        all_pass_judge(current_pass);
+        if (!current_pass && condition.not_pass_handler) {
+            condition.not_pass_handler();
+        }
+        if (current_pass && condition.pass_handler) {
+            condition.pass_handler();
         }
     }
+}
 
-    result.pass = function(){
-        var all_pass  = true;
-        var all_pass_judge = function(current_condition_pass){
-            all_pass = all_pass && current_condition_pass;
-            return all_pass;
-        }
-        this.go_through_conditions(all_pass_judge);
-        if(all_pass){
-            this.action();
-        };
+function AllConditions(){
+
+}
+
+AllConditions.prototype = new Conditions();
+
+AllConditions.prototype.pass = function(){
+    var all_pass  = true;
+    var all_pass_judge = function(current_condition_pass){
+        all_pass = all_pass && current_condition_pass;
+        return all_pass;
     }
+    this.go_through_conditions(all_pass_judge);
+    if(all_pass){
+        this.action();
+    };
+}
 
-    result.not_pass = function(){
-        var all_pass  = false;
-        var all_pass_judge = function(current_condition_pass){
-            all_pass = all_pass || current_condition_pass;
-            return all_pass;
-        }
-        this.go_through_conditions(all_pass_judge);
-
-        if(!all_pass){
-            this.action();
-        };
+AllConditions.prototype.not_pass = function(){
+    var all_pass  = false;
+    var all_pass_judge = function(current_condition_pass){
+        all_pass = all_pass || current_condition_pass;
+        return all_pass;
     }
+    this.go_through_conditions(all_pass_judge);
 
-    return result;
+    if(!all_pass){
+        this.action();
+    };
 }
